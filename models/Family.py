0821d85ff168cc181ec287e53b23c6a6906d1c2a
IncapableFury <41422991+IncapableFury@users.noreply.github.com>
2020-09-23 12:13:18 -0400

    tidy up

--- models/Family.py
+++ models/Family.py
@@ -1,7 +1,8 @@
-##from individual import individual
+from models.Individual import Individual
+
 
 class Family:
-    def __init__(self, id, husband, wife, married = None, divorced = None, children_list = []):
+    def __init__(self, id, husband, wife, married=None, divorced=None, children_list=[]):
         self.id = id
         self._marriedDate = married
         self._divorced = divorced
@@ -9,15 +10,15 @@ class Family:
         self._wife = wife
         self._children = children_list
 
-    def get_husband(self) -> individual:
+    def get_husband(self) -> Individual:
         return self._husband
 
-    def get_wife(self) -> individual:
+    def get_wife(self) -> Individual:
         return self._wife
 
     def get_marriedDate(self) -> str:
         return self._marriedDate
-    
+
     def get_divorced(self) -> str:
         return self._divorced
 
@@ -25,25 +26,33 @@ class Family:
         return self._children
 
     def set_husband(self, husband):
-        if not isinstance(husband, type(individual)): raise TypeError("input has to be a individual type")
+        if not isinstance(husband, type(Individual)): raise TypeError("input has to be a individual type")
         self._husband = husband
-    
+
     def set_wife(self, wife):
-        if not isinstance(wife, type(individual)): raise TypeError("input has to be a individual type")
+        if not isinstance(wife, type(Individual)): raise TypeError("input has to be a individual type")
         self._wife = wife
-    
+
     def set_marriedDate(self, married_date):
         if not isinstance(married_date, str): raise TypeError("input has to be a str type")
         self._marriedDate = married_date
-    
+
     def set_divorced(self, divorced_date):
         if not isinstance(divorced_date, str): raise TypeError("input has to be a str type")
         self._divorced = divorced_date
 
     def set_children(self, children_list):
-        if not isinstance(children_list, list) raise TypeError("input has to be a list type")
+        if not isinstance(children_list, list): raise TypeError("input has to be a list type")
         self._children = children_list
 
     def add_children(self, child):
-        if not isinstance(child, individual) raise TypeError("input has to be a individual type")
+        if not isinstance(child, Individual): raise TypeError("input has to be a individual type")
         self._children.append(child)
+
+    # ---------------------shit testing below---------------------
+
+    def siblings_spacing(self):
+        pass
+
+    def multiple_births_lessOrEqual_than_5(self):
+        pass
--- models/Individual.py
+++ models/Individual.py
@@ -13,3 +13,41 @@ class Individual:
 
     def get_name(self) -> str:
         return self._name if self._name else "NA"
+
+    # ---------------------shit testing below---------------------
+
+    def dates_before_current_date(self):
+        pass
+
+    def birth_before_marriage(self):
+        pass
+
+    def birth_before_death(self):
+        pass
+
+    def marriage_before_divorce(self):
+        pass
+
+    def marriage_before_death(self):
+        pass
+
+    def divorce_before_death(self):
+        pass
+
+    def less_then_150_years_old(self):
+        pass
+
+    def birth_before_marriage_of_parents(self):
+        pass
+
+    def birth_before_death_of_parents(self):
+        pass
+
+    def marriage_after_14(self):
+        pass
+
+    def no_bigamy(self):
+        pass
+
+    def parents_not_too_old(self):
+        pass
