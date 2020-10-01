ab1eb59b8c63b73cd15b1d98bdc8c2de3333837a
mfahim5 <33581164+mfahim5@users.noreply.github.com>
2020-10-01 16:27:10 -0400

    update with user stories

--- Testing.py
+++ Testing.py
@@ -4,18 +4,68 @@ Van comment for second time
 """
 
 import unittest
+from datetime import date 
 
-from models import Individual
-from models import Family
-from models import Gedcom
+from models.Individual import Individual
+from models.Family import Family
+from models.Gedcom import Gedcom
+
+
 
 
 class TestTriangles(unittest.TestCase):
 
-    def testInputValidation(self):
-        pass
+   
+    def testMarriageBeforeDivorce1(self):
+        self.assertTrue(marriage_before_divorce(fam1))
+
+    def testMarriageBeforeDivorce2(self):
+        self.assertEqual(marriage_before_divorce(fam1), True)
+
+    def testMarriageBeforeDivorce3(self):
+        self.assertNotEqual(marriage_before_divorce(fam1), False)
+
+    def testMarriageBeforeDivorce4(self):
+        self.assertIsNot(marriage_before_divorce(fam1), " ")
+
+    def testMarriageBeforeDivorce5(self):
+        self.assertIsNotNone(marriage_before_divorce(fam1))
+
+    def testMarriageBeforeDeath1(self):
+        self.assertTrue(fam2.marriage_before_death())
+
+    def testMarriageBeforeDeath2(self):
+        self.assertEqual(fam2.marriage_before_death(), True)
+
+    def testMarriageBeforeDeath3(self):
+        self.assertNotEqual(fam2.marriage_before_death(), False)
+
+        
+def marriage_before_divorce(Family):
+    from datetime import date
+    marriage= Family.get_marriedDate()
+    divorce= Family.get_divorced()
+    timediff = date(*marriage)-date(*divorce)
+    if timediff.days <0:
+        return True
+    print("Error marriage before divorce: Marriage date of "+Family.get_id+" happened after the divorce date.")
+    return False
 
 
 if __name__ == '__main__':
+    # -----------------------------------------------
+    fam1=Family("F01")
+    fam1.set_divorced((2009,4,5))
+    fam1.set_marriedDate((2000,8,9))
+    fam2=Family("F02")
+    male1=Individual("P01")
+    per1=Individual("P01")
+    per2=Individual("P02")
+    per1.set_deathDate("12 APR 2010")
+    per2.set_deathDate("15 MAY 2011")
+    fam2.set_husband(per1)
+    fam2.set_wife(per2)
+    fam2.set_marriedDate((2001,9,12))
+    #-------------------------------------------------
     print('Running unit tests')
     unittest.main()
--- models/Family.py
+++ models/Family.py
@@ -45,12 +45,12 @@ class Family:
     def set_marriedDate(self, married_date: str) -> None:
         
         ##if not isinstance(married_date, str): raise TypeError("input has to be a str type")
-        self._marriedDate = self.change_date_formate(married_date)
+        self._marriedDate = married_date
     
     def set_divorced(self, divorced_date: str) -> None:
         
         ##if not isinstance(divorced_date, str): raise TypeError("input has to be a str type")
-        self._divorced = self.change_date_formate(divorced_date)
+        self._divorced = divorced_date
 
     def set_children(self, children_list: list) -> None:
         ##if not isinstance(children_list, list): raise TypeError("input has to be a list type")
@@ -82,7 +82,14 @@ class Family:
         pass
 
     def marriage_before_divorce(self):
-        pass
+        from datetime import date
+        marriage= self.get_marriedDate()
+        divorce= self.get_divorced()
+        timedelta = date(*marriage)-date(*divorce)
+        if timedelta.days <0:
+            return True
+        print("Error marriage before divorce: Marriage date of "+Family.get_id+" happened after the divorce date.")
+        return False
 
     def dates_before_current_date(self):
         pass
@@ -91,7 +98,19 @@ class Family:
         pass
 
     def marriage_before_death(self):
-        pass
+        from datetime import date
+        from models.Individual import Individual
+        marriage=self.get_marriedDate()
+        if self._husband.get_deathDate() > self._wife.get_deathDate():
+            death= self._wife.get_deathDate()
+        else:
+            death= self._husband.get_deathDate()
+        timedelta=date(*marriage)-date(*death)
+        if timedelta.days<0:
+            return  True
+        print("Error marriage before death: Marriage date of "+Family.get_id+" happened after they died.")
+        return False
+        
 
     def divorce_before_death(self):
         pass
