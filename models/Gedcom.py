10aea7ddf901ea997d4df3fc76c1f41db5b87818
39xdgy <39xdgy@gmail.com>
2020-11-21 22:30:33 -0500

    add config class for cross platform

--- models/Gedcom.py
+++ models/Gedcom.py
@@ -1,5 +1,7 @@
 from models.Individual import Individual
 from models.Family import Family
+from models.report_error import report_error
+import config
 
 class Gedcom:
 
@@ -8,6 +10,7 @@ class Gedcom:
         self._individuals = {}
         self._families = {}
         self._data = self.readfile(path)
+        config.error_report = report_error()
 
     def readfile(self, path):
         """
